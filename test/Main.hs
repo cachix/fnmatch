bec49bc668d10233a93c1c954c27206624e18c39
Domen Kožar <domen@dev.si>
2024-03-02 03:21:52 +0000

    Test for #1

--- test/Main.hs
+++ test/Main.hs
@@ -1,23 +1,24 @@
 module Main (main) where
 
+import FnMatch qualified
 import Test.Hspec
 
-import qualified FnMatch
-
-
 main :: IO ()
-main = do 
-    hspec $ do
-      describe "FnMatch" $ do
-        it "matches a pattern" $ do
-          FnMatch.fnmatch "abc" "abc" [] `shouldBe` True
-        it "doesn't match a pattern" $ do
-          FnMatch.fnmatch "abc" "def" [] `shouldBe` False
-        it "matches a pattern with a ?" $ do
-          FnMatch.fnmatch "a?c" "abc" [] `shouldBe` True
-        it "matches a pattern with a *" $ do
-          FnMatch.fnmatch "a*c" "abc" [] `shouldBe` True
-        -- we pass a FlagCaseFold to fnmatch 
-        it "matches a pattern with a case fold flag" $ do
-          FnMatch.fnmatch "a*c" "ABC" [FnMatch.FlagCaseFold] `shouldBe` True
-          FnMatch.fnmatch "a*c" "ABC" [] `shouldBe` False
+main = do
+  hspec $ do
+    describe "FnMatch" $ do
+      it "matches a pattern" $ do
+        FnMatch.fnmatch "abc" "abc" [] `shouldBe` True
+      it "doesn't match a pattern" $ do
+        FnMatch.fnmatch "abc" "def" [] `shouldBe` False
+      it "matches a pattern with a ?" $ do
+        FnMatch.fnmatch "a?c" "abc" [] `shouldBe` True
+      it "matches a pattern with a *" $ do
+        FnMatch.fnmatch "a*c" "abc" [] `shouldBe` True
+      -- we pass a FlagCaseFold to fnmatch
+      it "matches a pattern with a case fold flag" $ do
+        FnMatch.fnmatch "a*c" "ABC" [FnMatch.FlagCaseFold] `shouldBe` True
+        FnMatch.fnmatch "a*c" "ABC" [] `shouldBe` False
+      it "respects referential transparency" $ do
+        FnMatch.fnmatch "a" "a" [] `shouldBe` True
+        FnMatch.fnmatch "a" "a" [] `shouldBe` True
